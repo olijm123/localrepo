c631463e095f10b29ee477943bf2767a064ddc5d
Oliver Mizen <oliverjtest@gmail.com>
2023-12-28 16:35:50 +0000

    Adding stuff

---
+++ python-lesson.py
@@ -0,0 +1,12 @@
+#print("hello world")
+#print(25 * 68 + 13 / 28)
+exponented_variable = 2 ** 4
+modulo_variable = 14 % 4
+#print(exponented_variable)
+#print(modulo_variable)
+total_cost = 5
+total_cost += 10
+print(total_cost)
+cool_number = 12 + 30
+cool_number * 5
+print(cool_number)
